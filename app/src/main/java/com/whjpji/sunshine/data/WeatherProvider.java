ca71d245b9545eb06deec01ab147e231c140432f
whjpji <254521090@qq.com>
2016-08-23 21:54:50 +0800

    Revise the package name, and implement the getType method in WeatherProvider

--- app/src/main/java/com/whjpji/sunshine/data/WeatherProvider.java
+++ app/src/main/java/com/whjpji/sunshine/data/WeatherProvider.java
@@ -157,10 +157,12 @@ public class WeatherProvider extends ContentProvider {
 
         switch (match) {
             // Student: Uncomment and fill out these two cases
-//            case WEATHER_WITH_LOCATION_AND_DATE:
-//            case WEATHER_WITH_LOCATION:
             case WEATHER:
                 return WeatherContract.WeatherEntry.CONTENT_TYPE;
+            case WEATHER_WITH_LOCATION:
+                return WeatherContract.WeatherEntry.CONTENT_TYPE;
+            case WEATHER_WITH_LOCATION_AND_DATE:
+                return WeatherContract.WeatherEntry.CONTENT_ITEM_TYPE;
             case LOCATION:
                 return WeatherContract.LocationEntry.CONTENT_TYPE;
             default:

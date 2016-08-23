3577acdbf70757510a9f57e54cd01bc1c765a64c
whjpji <254521090@qq.com>
2016-08-23 23:25:15 +0800

    Implement and test the delete and update method of WeatherProvider.

--- app/src/main/java/com/whjpji/sunshine/data/WeatherProvider.java
+++ app/src/main/java/com/whjpji/sunshine/data/WeatherProvider.java
@@ -259,23 +259,51 @@ public class WeatherProvider extends ContentProvider {
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
         getContext().getContentResolver().notifyChange(uri, null);
+        db.close();
         return returnUri;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // Student: Start by getting a writable database
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
 
         // Student: Use the uriMatcher to match the WEATHER and LOCATION URI's we are going to
         // handle.  If it doesn't match these, throw an UnsupportedOperationException.
+        final int match = sUriMatcher.match(uri);
 
+        int rowsDeleted = 0;
         // Student: A null value deletes all rows.  In my implementation of this, I only notified
         // the uri listeners (using the content resolver) if the rowsDeleted != 0 or the selection
         // is null.
+        if (selection == null) selection = "1";
+        switch (match) {
+            case WEATHER:
+                rowsDeleted = db.delete(
+                        WeatherContract.WeatherEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs
+                );
+                break;
+            case LOCATION:
+                rowsDeleted = db.delete(
+                        WeatherContract.LocationEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
         // Oh, and you should notify the listeners here.
+        // Only notify changes when it exactly changes.
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
 
         // Student: return the actual rows deleted
-        return 0;
+        db.close();
+        return rowsDeleted;
     }
 
     private void normalizeDate(ContentValues values) {
@@ -291,7 +319,46 @@ public class WeatherProvider extends ContentProvider {
             Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         // Student: This is a lot like the delete function.  We return the number of rows impacted
         // by the update.
-        return 0;
+
+        // Get the writable database.
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+        // Using the Uri matcher to get the uri type.
+        final int match = sUriMatcher.match(uri);
+
+        int rowsUpdated = 0;
+
+        // Do update for each type of uri.
+        switch (match) {
+            case WEATHER:
+                // Always normalize date value first.
+                normalizeDate(values);
+                rowsUpdated = db.update(
+                        WeatherContract.WeatherEntry.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs
+                );
+                break;
+            case LOCATION:
+                rowsUpdated = db.update(
+                        WeatherContract.LocationEntry.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // Notify the data changes.
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        // Close the database.
+        db.close();
+        return rowsUpdated;
     }
 
     @Override

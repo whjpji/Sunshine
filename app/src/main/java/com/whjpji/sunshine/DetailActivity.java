601584615d5d5717d46ae270d6638cab720b421f
whjpji <254521090@qq.com>
2016-08-24 20:59:22 +0800

    Format the temperature string.

--- app/src/main/java/com/whjpji/sunshine/DetailActivity.java
+++ app/src/main/java/com/whjpji/sunshine/DetailActivity.java
@@ -203,9 +203,13 @@ public class DetailActivity extends AppCompatActivity {
 
             boolean isMetric = Utility.isMetric(getActivity());
 
-            String high = Utility.formatTemperature(data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+            String high = Utility.formatTemperature(
+                    getActivity(), data.getDouble(COL_WEATHER_MAX_TEMP), isMetric
+            );
 
-            String low = Utility.formatTemperature(data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+            String low = Utility.formatTemperature(
+                    getActivity(), data.getDouble(COL_WEATHER_MIN_TEMP), isMetric
+            );
 
             mForecast = String.format("%s - %s - %s/%s", dateString, weatherDescription, high, low);
 

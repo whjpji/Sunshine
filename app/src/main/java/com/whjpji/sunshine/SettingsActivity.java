8187b1ab859fca08ceb5503c3ef567e71e93e575
whjpji <254521090@qq.com>
2016-08-26 22:27:59 +0800

    Implement the red lines of the list item layout of the phone.

--- app/src/main/java/com/whjpji/sunshine/SettingsActivity.java
+++ app/src/main/java/com/whjpji/sunshine/SettingsActivity.java
@@ -97,7 +97,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setupActionBar();
+        // setupActionBar();
     }
 
     /**
